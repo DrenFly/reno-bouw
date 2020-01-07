c510e168e54b4a535ebf9faee98614fd2150ab2c
Ailín Draighneán <drenfly@pm.me>
2020-01-07 18:42:44 +0000

    Gallery 90% done

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+// const scroll = document.querySelector(".header-home");
+
+
+// window.onscroll = (e) => {
+//     if 
+// }
+
+
+// This is the event "onscroll" on the window object, that we're binding to and passing an
+// anonymous function as the function to be executed when the event is triggered
+// window.onscroll = function (event) {
+//     // Then here (inside the anonymous function that's being passed in), we add the class
+//     // "my-class" to the document body.
+//     document.body.classList.add("my-class");
+//   }
